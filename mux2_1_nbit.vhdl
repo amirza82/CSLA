76d6cc3bd1fd3d3f33ebedba05d0404d3fa1519b
amirza82 <zahabiareza@gmail.com>
2025-04-07 14:30:33 +0330

    mux completed

--- mux2_1_nbit.vhdl
+++ mux2_1_nbit.vhdl
@@ -1,8 +1,32 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
 entity mux2_1_nbit is 
-	generic n: integer;
+	generic (n: integer := 2);
 	port (
-	a : in std_logic_vector;
-	b : in std_logic_vector;
-	s : in std_logic;
-	b : out std_logic_vector;
+	a : in  std_logic_vector (n-1 downto 0);
+	b : in  std_logic_vector (n-1 downto 0);
+	s : in  std_logic;
+	o : out std_logic_vector (n-1 downto 0)
 	     );
+end mux2_1_nbit;
+
+architecture structural of mux2_1_nbit is
+begin
+	o <= a when s='0' else
+	     b;
+end structural;
+
+entity mux2_1_nbit_tb is
+end mux2_1_nbit_tb;
+
+architecture behavioral of mux2_1_nbit_tb is
+	component mux2_1_nbit
+	generic (n: integer);
+	port (
+	a : in  std_logic_vector (n-1 downto 0);
+	b : in  std_logic_vector (n-1 downto 0);
+	s : in  std_logic;
+	o : out std_logic_vector (n-1 downto 0)
+	     );
+end behavioral;
