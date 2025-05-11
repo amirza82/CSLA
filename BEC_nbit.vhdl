0e1f375d7fe55784008e08b14e035ace36b1a3b2
amirza82 <zahabiareza@gmail.com>
2025-05-11 11:07:12 +0330

    BEC in one file

--- BEC_nbit.vhdl
+++ BEC_nbit.vhdl
@@ -1,5 +1,21 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+
+entity port_bec_1bit is port (
+    A,B : in std_logic;
+    C,F : out std_logic
+);
+end port_bec_1bit;
+
+
+architecture bec_1bit of port_bec_1bit is 
+begin
+    C <= B xor A;
+    F <= B and A;
+end bec_1bit;
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 entity port_bec_nbit is
@@ -30,3 +46,60 @@ begin
     X(n-1)<=bit_transfer(n-2) xor B(n-1);
 
 end bec_nbit;
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity tb_port_bec_nbit is 
+end tb_port_bec_nbit;
+
+    architecture behavior of tb_port_bec_nbit is
+        component port_bec_nbit
+            generic (
+                n : integer := 4              );
+            port (
+                B : in std_logic_vector(n-1 downto 0);
+                X : out std_logic_vector(n-1 downto 0)
+            );
+        end component;
+
+
+        constant N_BITS : integer := 4;
+        signal B_tb : std_logic_vector(N_BITS-1 downto 0);
+        signal X_tb : std_logic_vector(N_BITS-1 downto 0);
+
+    begin
+        uut: port_bec_nbit
+        generic map (
+            n => N_BITS
+        )
+        port map (
+            B => B_tb,
+            X => X_tb
+        );
+
+        stim_proc: process
+        begin
+
+            B_tb <= "0000";
+            wait for 10 ns;
+
+            B_tb <= "0001";
+            wait for 10 ns;
+
+            B_tb <= "0010";
+            wait for 10 ns;
+
+            B_tb <= "0101";
+            wait for 10 ns;
+
+            B_tb <= "0100";
+            wait for 10 ns;
+
+            B_tb <= "1111";
+            wait for 10 ns;
+            wait;
+        end process;
+
+    end behavior;
